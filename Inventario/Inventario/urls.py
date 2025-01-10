c1fc53b982a014342c6a848b9901b6d16355ea00
Tayron Morales <169932075+Tayron2024@users.noreply.github.com>
2025-01-10 07:33:33 -0500

    Update urls.py

--- Inventario/Inventario/urls.py
+++ Inventario/Inventario/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
-
-from django.contrib import admin
 from django.urls import path
-
-from InvRestaurante import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.Inventario, name= 'Inventario.urls'),
-    path('', views.Principal, name= 'Principal.urls'),
+    # Ruta raíz, carga Principal.html
+    path('', TemplateView.as_view(template_name="Principal.html"), name="principal"),
+
+    # Ruta para Inventario.html
+    path('inventario/', TemplateView.as_view(template_name="Inventario.html"), name="inventario"),
 ]
+
